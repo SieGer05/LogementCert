521f5321f1baddff987b084394dc4b2183cb3f58
DJILI Mohamed Amine <aminedjili29@gmail.com>
2025-05-25 13:17:54 +0100

    Fix generate keys method

--- Crypto/key_manager.py
+++ Crypto/key_manager.py
@@ -26,13 +26,13 @@ class KeyManager:
       )
       public_key = private_key.public_key()
       
-      private_pem = private_key.serialize(
+      private_pem = private_key.private_bytes(
          encoding=serialization.Encoding.PEM,
          format=serialization.PrivateFormat.PKCS8,
          encryption_algorithm=serialization.NoEncryption()
       ).decode('utf-8')
 
-      public_pem = public_key.serialize(
+      public_pem = public_key.public_bytes(
          encoding=serialization.Encoding.PEM,
          format=serialization.PublicFormat.SubjectPublicKeyInfo
       ).decode('utf-8')
